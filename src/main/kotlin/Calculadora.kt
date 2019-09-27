b723a86c6678ced5dd3f9a9ba050973c02d5ba96
LizziA <lizzi.aguilar@ujcv.edu.hn>
2019-09-27 15:25:44 -0600

    Kata String Calculator Tarea

--- src/main/kotlin/Calculadora.kt
+++ src/main/kotlin/Calculadora.kt
@@ -1,16 +1,40 @@
-
-import java.util.StringTokenizer
-
 class Calculadora {
     fun suma(s: String = ""): Int {
         if (s.isNullOrEmpty())
             return 0
-        val split = s.split("," , "\n")
-        val newline = System.getProperty("line.separator")
+
+        val split = s.split(",")
         var total = 0
         for (num in split) {
             total = total + num.toInt()
         }
+
         return total
+    }
+
+    fun sumad(r: String = ""): Int {
+        val five = r.split("\n")
+        var t = 0
+        for (N in five) {
+            t = t + N.toInt()
+        }
+        return t
+    }
+
+    fun sumap(p: String = ""): Int {
+        val seis = p.split(";//")
+        var suma = 0
+        for (Numero in seis) {
+            suma = suma+ Numero.toInt()
+        }
+        return suma
+    }
+    fun sumac(c: String = ""): Int {
+        val ten = c.split("///*,")
+        var sum = 0
+        for (Nu in ten) {
+            sum = sum + Nu.toInt()
+        }
+        return sum
     }
 }
--- src/test/kotlin/CalculatorSpec.kt
+++ src/test/kotlin/CalculatorSpec.kt
@@ -3,55 +3,68 @@ import io.kotlintest.specs.WordSpec
 
 class CalculatorSpec : WordSpec(){
     init {
-        "Si la entrada esta vacia" should  {
-             "deberia retornar 0" {
-                 var sistemaBajoPrueba = Calculadora()
-                 var resultado = sistemaBajoPrueba.suma()
-                 // prueba
-                 resultado.shouldBe (0)
-             }
-         }
-        "Si la entrada es un numero" should {
-             "deberia regresar ese numero" {
-                 var sistemaBajoPrueba = Calculadora()
-                 var resultado = sistemaBajoPrueba.suma("2")
-                 // prueba
-                 resultado.shouldBe (2)
-             }
-         }
-        "Si la entrada son 2 numeros" should  {
-             "deberia regresar las suma de los 2 numeros" {
-                 var sistemaBajoPrueba = Calculadora()
-                 var resultado = sistemaBajoPrueba.suma("1,2")
-                 // prueba
-                 resultado.shouldBe (3)
-             }
-         }
-        "Si la entrada es un numero desconocido" should  {
-             "deberia regresar la suma" {
-                 var sistemaBajoPrueba = Calculadora()
-                 var resultado = sistemaBajoPrueba.suma("1,2,3,4")
-                 // prueba
-                 resultado.shouldBe (10)
-             }
-         }
-        "Si la entrada son multiples numeros delimitados por nuevas lineas" should  {
-             "deberia regresar la suma" {
-                 var sistemaBajoPrueba = Calculadora()
-                 var resultado = sistemaBajoPrueba.suma("1\n 2,3")
-                 // prueba
-                 resultado.shouldBe (6)
-             }
-         }
-    }
-}
+        "Si la entrada es vacia" should {
+            "deberia retornar 0" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.suma()
+                // prueba
+                resultado.shouldBe(0)
+            }
+        }
 
-//    1. Dado que la entrada del usuario es vacia debe retornar cero.
-//    2. Si la entrada del usaurio es un solo numero debe regresar ese numero
-//    3. Si la entrada del usaurio son dos numeros separados por coma debe regresar la suma de los dos numeros
-//    4. Si la entrada del usuario es un numero desconocido de numeros separados por coma debe regresar la suma
-//    5. Si la entrada del usuario son multiples numeros que usan comas y nuevas lineas como delimitadores debe regresar la suma de los numeros
-//    6. Si la entrada del usuario son varios numeros separados por un delimitador personalizado debe regresar la suma de los numeros (ejemplo “//;\n1;2” debe retornar 3)
+        "Si la entrada es solo 1 numero" should {
+            "deberia retornar ese numero" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.suma("3")
+                // prueba
+                resultado.shouldBe(3)
+            }
+        }
+        "Si la entrada son 2 numeros separados por coma" should {
+            "deberia regresar la suma de los 2 numeros" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.suma("3,4")
+                // prueba
+                resultado.shouldBe(7)
+            }
+        }
+
+        "Si la entrada son 2 numeros desconocidos separados por coma" should {
+            "deberia regresar la suma $" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.suma("1,2,3,4,5,6")
+                // prueba
+                resultado.shouldBe(21)
+            }
+        }
+        " Si la entrada son multiples numeros separados por nueva lineas " should {
+            "deberia retornar suma" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.sumad("1\n2\n3")
+                // prueba
+                resultado.shouldBe(6)
+            }
+        }
+        "Si la entrada son varios numeros separados por un delimitador personalizado" should {
+            "deberia regresar la suma" {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.sumap("1;//2;//3;//4;//5;//6")
+                // prueba
+                resultado.shouldBe(21)
+            }
+        }
+        "Si la entrada son multiples numeros separados por un delimitador multicaracter personalizado" should {
+            "deberia regresar la suma " {
+                var sistemaBajoPrueba = Calculadora()
+                var resultado = sistemaBajoPrueba.sumac("1///*,2///*,3///*,4")
+                // prueba
+                resultado.shouldBe(10)
+
+            }
+        }
+    }
+    }
+
 //    7. Si la entrada del usuario contiene un numero negativo debe retornar una excepcion con el mensaje "negativos no permitidos: x" donde x es el negativo
 //    8. Si la entrada del usuario contiene multiples negativos debe regresar una excepcion con el mensaje "negativos no permitidos: -x, -y, -z  donde x,y,z son negativos
 //    9. Si la entrada del usuario contiene numeros mayores a 1000 debe regresar la suma de los numeros menores a 1001
